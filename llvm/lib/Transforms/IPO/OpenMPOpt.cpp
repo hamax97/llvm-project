997cb9334a84b4ceab2b88ee6b819e2d249b7bff
Hamilton Tobon Mosquera <Hamilton Tobon>
2020-07-07 11:06:01 -0400

    Exposing OMPInformationCache for unittesting

--- llvm/lib/Transforms/IPO/OpenMPOpt.cpp
+++ llvm/lib/Transforms/IPO/OpenMPOpt.cpp
@@ -13,6 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/IPO/OpenMPOpt.h"
+#include "OpenMPOptPriv.h"
 
 #include "llvm/ADT/EnumeratedArray.h"
 #include "llvm/ADT/Statistic.h"
@@ -95,152 +96,7 @@ namespace {
 
 struct AAICVTracker;
 
-/// OpenMP specific information. For now, stores RFIs and ICVs also needed for
-/// Attributor runs.
-struct OMPInformationCache : public InformationCache {
-  OMPInformationCache(Module &M, AnalysisGetter &AG,
-                      BumpPtrAllocator &Allocator, SetVector<Function *> *CGSCC,
-                      SmallPtrSetImpl<Function *> &ModuleSlice)
-      : InformationCache(M, AG, Allocator, CGSCC), ModuleSlice(ModuleSlice),
-        OMPBuilder(M) {
-    OMPBuilder.initialize();
-    initializeRuntimeFunctions();
-    initializeInternalControlVars();
-  }
-
-  /// Generic information that describes an internal control variable.
-  struct InternalControlVarInfo {
-    /// The kind, as described by InternalControlVar enum.
-    InternalControlVar Kind;
-
-    /// The name of the ICV.
-    StringRef Name;
-
-    /// Environment variable associated with this ICV.
-    StringRef EnvVarName;
-
-    /// Initial value kind.
-    ICVInitValue InitKind;
-
-    /// Initial value.
-    ConstantInt *InitValue;
-
-    /// Setter RTL function associated with this ICV.
-    RuntimeFunction Setter;
-
-    /// Getter RTL function associated with this ICV.
-    RuntimeFunction Getter;
-
-    /// RTL Function corresponding to the override clause of this ICV
-    RuntimeFunction Clause;
-  };
-
-  /// Generic information that describes a runtime function
-  struct RuntimeFunctionInfo {
-
-    /// The kind, as described by the RuntimeFunction enum.
-    RuntimeFunction Kind;
-
-    /// The name of the function.
-    StringRef Name;
-
-    /// Flag to indicate a variadic function.
-    bool IsVarArg;
-
-    /// The return type of the function.
-    Type *ReturnType;
-
-    /// The argument types of the function.
-    SmallVector<Type *, 8> ArgumentTypes;
-
-    /// The declaration if available.
-    Function *Declaration = nullptr;
-
-    /// Uses of this runtime function per function containing the use.
-    using UseVector = SmallVector<Use *, 16>;
-
-    /// Return the vector of uses in function \p F.
-    UseVector &getOrCreateUseVector(Function *F) {
-      std::shared_ptr<UseVector> &UV = UsesMap[F];
-      if (!UV)
-        UV = std::make_shared<UseVector>();
-      return *UV;
-    }
-
-    /// Return the vector of uses in function \p F or `nullptr` if there are
-    /// none.
-    const UseVector *getUseVector(Function &F) const {
-      auto I = UsesMap.find(&F);
-      if (I != UsesMap.end())
-        return I->second.get();
-      return nullptr;
-    }
-
-    /// Return how many functions contain uses of this runtime function.
-    size_t getNumFunctionsWithUses() const { return UsesMap.size(); }
-
-    /// Return the number of arguments (or the minimal number for variadic
-    /// functions).
-    size_t getNumArgs() const { return ArgumentTypes.size(); }
-
-    /// Run the callback \p CB on each use and forget the use if the result is
-    /// true. The callback will be fed the function in which the use was
-    /// encountered as second argument.
-    void foreachUse(function_ref<bool(Use &, Function &)> CB) {
-      for (auto &It : UsesMap)
-        foreachUse(CB, It.first, It.second.get());
-    }
-
-    /// Run the callback \p CB on each use within the function \p F and forget
-    /// the use if the result is true.
-    void foreachUse(function_ref<bool(Use &, Function &)> CB, Function *F,
-                    UseVector *Uses = nullptr) {
-      SmallVector<unsigned, 8> ToBeDeleted;
-      ToBeDeleted.clear();
-
-      unsigned Idx = 0;
-      UseVector &UV = Uses ? *Uses : getOrCreateUseVector(F);
-
-      for (Use *U : UV) {
-        if (CB(*U, *F))
-          ToBeDeleted.push_back(Idx);
-        ++Idx;
-      }
-
-      // Remove the to-be-deleted indices in reverse order as prior
-      // modifcations will not modify the smaller indices.
-      while (!ToBeDeleted.empty()) {
-        unsigned Idx = ToBeDeleted.pop_back_val();
-        UV[Idx] = UV.back();
-        UV.pop_back();
-      }
-    }
-
-  private:
-    /// Map from functions to all uses of this runtime function contained in
-    /// them.
-    DenseMap<Function *, std::shared_ptr<UseVector>> UsesMap;
-  };
-
-  /// The slice of the module we are allowed to look at.
-  SmallPtrSetImpl<Function *> &ModuleSlice;
-
-  /// An OpenMP-IR-Builder instance
-  OpenMPIRBuilder OMPBuilder;
-
-  /// Map from runtime function kind to the runtime function description.
-  EnumeratedArray<RuntimeFunctionInfo, RuntimeFunction,
-                  RuntimeFunction::OMPRTL___last>
-      RFIs;
-
-  /// Map from ICV kind to the ICV description.
-  EnumeratedArray<InternalControlVarInfo, InternalControlVar,
-                  InternalControlVar::ICV___last>
-      ICVs;
-
-  /// Helper to initialize all internal control variable information for those
-  /// defined in OMPKinds.def.
-  void initializeInternalControlVars() {
+void OMPInformationCache::initializeInternalControlVars() {
 #define ICV_RT_SET(_Name, RTL)                                                 \
   {                                                                            \
     auto &ICV = ICVs[_Name];                                                   \
@@ -274,63 +130,58 @@ struct OMPInformationCache : public InformationCache {
     }                                                                          \
   }
 #include "llvm/Frontend/OpenMP/OMPKinds.def"
+}
+
+bool OMPInformationCache::declMatchesRTFTypes(
+    Function *F, Type *RTFRetType, SmallVector<Type *, 8> &RTFArgTypes) {
+  // TODO: We should output information to the user (under debug output
+  //       and via remarks).
+
+  if (!F)
+    return false;
+  if (F->getReturnType() != RTFRetType)
+    return false;
+  if (F->arg_size() != RTFArgTypes.size())
+    return false;
+
+  auto RTFTyIt = RTFArgTypes.begin();
+  for (Argument &Arg : F->args()) {
+    if (Arg.getType() != *RTFTyIt)
+      return false;
+
+    ++RTFTyIt;
   }
 
-  /// Returns true if the function declaration \p F matches the runtime
-  /// function types, that is, return type \p RTFRetType, and argument types
-  /// \p RTFArgTypes.
-  static bool declMatchesRTFTypes(Function *F, Type *RTFRetType,
-                                  SmallVector<Type *, 8> &RTFArgTypes) {
-    // TODO: We should output information to the user (under debug output
-    //       and via remarks).
+  return true;
+}
 
-    if (!F)
-      return false;
-    if (F->getReturnType() != RTFRetType)
-      return false;
-    if (F->arg_size() != RTFArgTypes.size())
-      return false;
+void OMPInformationCache::initializeRuntimeFunctions() {
+  // Helper to collect all uses of the decleration in the UsesMap.
+  auto CollectUses = [&](RuntimeFunctionInfo &RFI) {
+    unsigned NumUses = 0;
+    if (!RFI.Declaration)
+      return NumUses;
+    OMPBuilder.addAttributes(RFI.Kind, *RFI.Declaration);
 
-    auto RTFTyIt = RTFArgTypes.begin();
-    for (Argument &Arg : F->args()) {
-      if (Arg.getType() != *RTFTyIt)
-        return false;
+    NumOpenMPRuntimeFunctionsIdentified += 1;
+    NumOpenMPRuntimeFunctionUsesIdentified += RFI.Declaration->getNumUses();
 
-      ++RTFTyIt;
-    }
-
-    return true;
-  }
-
-  /// Helper to initialize all runtime function information for those defined
-  /// in OpenMPKinds.def.
-  void initializeRuntimeFunctions() {
-    // Helper to collect all uses of the decleration in the UsesMap.
-    auto CollectUses = [&](RuntimeFunctionInfo &RFI) {
-      unsigned NumUses = 0;
-      if (!RFI.Declaration)
-        return NumUses;
-      OMPBuilder.addAttributes(RFI.Kind, *RFI.Declaration);
-
-      NumOpenMPRuntimeFunctionsIdentified += 1;
-      NumOpenMPRuntimeFunctionUsesIdentified += RFI.Declaration->getNumUses();
-
-      // TODO: We directly convert uses into proper calls and unknown uses.
-      for (Use &U : RFI.Declaration->uses()) {
-        if (Instruction *UserI = dyn_cast<Instruction>(U.getUser())) {
-          if (ModuleSlice.count(UserI->getFunction())) {
-            RFI.getOrCreateUseVector(UserI->getFunction()).push_back(&U);
-            ++NumUses;
-          }
-        } else {
-          RFI.getOrCreateUseVector(nullptr).push_back(&U);
+    // TODO: We directly convert uses into proper calls and unknown uses.
+    for (Use &U : RFI.Declaration->uses()) {
+      if (Instruction *UserI = dyn_cast<Instruction>(U.getUser())) {
+        if (ModuleSlice.count(UserI->getFunction())) {
+          RFI.getOrCreateUseVector(UserI->getFunction()).push_back(&U);
           ++NumUses;
         }
+      } else {
+        RFI.getOrCreateUseVector(nullptr).push_back(&U);
+        ++NumUses;
       }
-      return NumUses;
-    };
+    }
+    return NumUses;
+  };
 
-    Module &M = *((*ModuleSlice.begin())->getParent());
+  Module &M = *((*ModuleSlice.begin())->getParent());
 
 #define OMP_RTL(_Enum, _Name, _IsVarArg, _ReturnType, ...)                     \
   {                                                                            \
@@ -358,9 +209,8 @@ struct OMPInformationCache : public InformationCache {
   }
 #include "llvm/Frontend/OpenMP/OMPKinds.def"
 
-    // TODO: We should attach the attributes defined in OMPKinds.def.
-  }
-};
+  // TODO: We should attach the attributes defined in OMPKinds.def.
+}
 
 struct OpenMPOpt {
 
---
+++ llvm/lib/Transforms/IPO/OpenMPOptPriv.h
@@ -0,0 +1,174 @@
+#ifndef LLVM_TRANSFORMS_IPO_OPENMP_OPT_PRIV_H
+#define LLVM_TRANSFORMS_IPO_OPENMP_OPT_PRIV_H
+
+#include "llvm/ADT/EnumeratedArray.h"
+#include "llvm/Transforms/IPO/Attributor.h"
+#include "llvm/Frontend/OpenMP/OMPConstants.h"
+#include "llvm/Frontend/OpenMP/OMPIRBuilder.h"
+
+using namespace llvm;
+using namespace omp;
+
+
+namespace {
+/// OpenMP specific information. For now, stores RFIs and ICVs also needed for
+/// Attributor runs.
+struct OMPInformationCache : public InformationCache {
+  OMPInformationCache(Module &M, AnalysisGetter &AG,
+                      BumpPtrAllocator &Allocator, SetVector<Function *> *CGSCC,
+                      SmallPtrSetImpl<Function *> &ModuleSlice)
+      : InformationCache(M, AG, Allocator, CGSCC), ModuleSlice(ModuleSlice),
+        OMPBuilder(M) {
+    OMPBuilder.initialize();
+    initializeRuntimeFunctions();
+    initializeInternalControlVars();
+  }
+
+  /// Generic information that describes an internal control variable.
+  struct InternalControlVarInfo {
+    /// The kind, as described by InternalControlVar enum.
+    InternalControlVar Kind;
+
+    /// The name of the ICV.
+    StringRef Name;
+
+    /// Environment variable associated with this ICV.
+    StringRef EnvVarName;
+
+    /// Initial value kind.
+    ICVInitValue InitKind;
+
+    /// Initial value.
+    ConstantInt *InitValue;
+
+    /// Setter RTL function associated with this ICV.
+    RuntimeFunction Setter;
+
+    /// Getter RTL function associated with this ICV.
+    RuntimeFunction Getter;
+
+    /// RTL Function corresponding to the override clause of this ICV
+    RuntimeFunction Clause;
+  };
+
+  /// Generic information that describes a runtime function
+  struct RuntimeFunctionInfo {
+
+    /// The kind, as described by the RuntimeFunction enum.
+    RuntimeFunction Kind;
+
+    /// The name of the function.
+    StringRef Name;
+
+    /// Flag to indicate a variadic function.
+    bool IsVarArg;
+
+    /// The return type of the function.
+    Type *ReturnType;
+
+    /// The argument types of the function.
+    SmallVector<Type *, 8> ArgumentTypes;
+
+    /// The declaration if available.
+    Function *Declaration = nullptr;
+
+    /// Uses of this runtime function per function containing the use.
+    using UseVector = SmallVector<Use *, 16>;
+
+    /// Return the vector of uses in function \p F.
+    UseVector &getOrCreateUseVector(Function *F) {
+      std::shared_ptr<UseVector> &UV = UsesMap[F];
+      if (!UV)
+        UV = std::make_shared<UseVector>();
+      return *UV;
+    }
+
+    /// Return the vector of uses in function \p F or `nullptr` if there are
+    /// none.
+    const UseVector *getUseVector(Function &F) const {
+      auto I = UsesMap.find(&F);
+      if (I != UsesMap.end())
+        return I->second.get();
+      return nullptr;
+    }
+
+    /// Return how many functions contain uses of this runtime function.
+    size_t getNumFunctionsWithUses() const { return UsesMap.size(); }
+
+    /// Return the number of arguments (or the minimal number for variadic
+    /// functions).
+    size_t getNumArgs() const { return ArgumentTypes.size(); }
+
+    /// Run the callback \p CB on each use and forget the use if the result is
+    /// true. The callback will be fed the function in which the use was
+    /// encountered as second argument.
+    void foreachUse(function_ref<bool(Use &, Function &)> CB) {
+      for (auto &It : UsesMap)
+        foreachUse(CB, It.first, It.second.get());
+    }
+
+    /// Run the callback \p CB on each use within the function \p F and forget
+    /// the use if the result is true.
+    void foreachUse(function_ref<bool(Use &, Function &)> CB, Function *F,
+                    UseVector *Uses = nullptr) {
+      SmallVector<unsigned, 8> ToBeDeleted;
+      ToBeDeleted.clear();
+
+      unsigned Idx = 0;
+      UseVector &UV = Uses ? *Uses : getOrCreateUseVector(F);
+
+      for (Use *U : UV) {
+        if (CB(*U, *F))
+          ToBeDeleted.push_back(Idx);
+        ++Idx;
+      }
+
+      // Remove the to-be-deleted indices in reverse order as prior
+      // modifcations will not modify the smaller indices.
+      while (!ToBeDeleted.empty()) {
+        unsigned Idx = ToBeDeleted.pop_back_val();
+        UV[Idx] = UV.back();
+        UV.pop_back();
+      }
+    }
+
+  private:
+    /// Map from functions to all uses of this runtime function contained in
+    /// them.
+    DenseMap<Function *, std::shared_ptr<UseVector>> UsesMap;
+  };
+
+  /// The slice of the module we are allowed to look at.
+  SmallPtrSetImpl<Function *> &ModuleSlice;
+
+  /// An OpenMP-IR-Builder instance
+  OpenMPIRBuilder OMPBuilder;
+
+  /// Map from runtime function kind to the runtime function description.
+  EnumeratedArray<RuntimeFunctionInfo, RuntimeFunction,
+  RuntimeFunction::OMPRTL___last>
+      RFIs;
+
+  /// Map from ICV kind to the ICV description.
+  EnumeratedArray<InternalControlVarInfo, InternalControlVar,
+  InternalControlVar::ICV___last>
+      ICVs;
+
+  /// Helper to initialize all internal control variable information for those
+  /// defined in OMPKinds.def.
+  void initializeInternalControlVars();
+
+  /// Returns true if the function declaration \p F matches the runtime
+  /// function types, that is, return type \p RTFRetType, and argument types
+  /// \p RTFArgTypes.
+  static bool declMatchesRTFTypes(Function *F, Type *RTFRetType,
+                                  SmallVector<Type *, 8> &RTFArgTypes);
+
+  /// Helper to initialize all runtime function information for those defined
+  /// in OpenMPKinds.def.
+  void initializeRuntimeFunctions();
+};
+
+} // end anonymous namespace
+
+#endif // LLVM_TRANSFORMS_IPO_OPENMP_OPT_PRIV_H
