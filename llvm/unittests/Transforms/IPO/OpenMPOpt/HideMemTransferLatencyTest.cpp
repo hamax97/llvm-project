9f86305649063b397afdc92dead123dc4acd6e3e
Hamilton Tobon Mosquera <Hamilton Tobon>
2020-07-17 11:45:16 -0500

    Setting up the environment for the unittest

--- llvm/unittests/Transforms/IPO/OpenMPOpt/HideMemTransferLatencyTest.cpp
+++ llvm/unittests/Transforms/IPO/OpenMPOpt/HideMemTransferLatencyTest.cpp
@@ -12,32 +12,137 @@ namespace {
 
 class HideMemTransferLatencyTest : public OpenMPOptTest {
 protected:
-  /// This stores the runtime calls for further use within each test.
-  SmallVector<CallBase *, 16> MemTransferCalls;
+  NiceMock<MockFunctionPass> MFP;
+  NiceMock<MockModulePass> MMP;
+  MockSCCPass MSCCP;
+  ModulePassManager MPM;
 
-  void getCallSites(const char *ModuleString) {
-    auto M = parseModuleString(ModuleString);
-    initializeOMPInfoCache(*M);
+  FunctionAnalysisManager FAM;
+  ModuleAnalysisManager MAM;
+  CGSCCAnalysisManager CGAM;
 
-    auto &RFI = OMPInfoCache->RFIs[OMPRTL___tgt_target_data_begin];
-    auto GetCallSites = [&](Use &U, Function &Decl) {
-      auto *RTCall = OpenMPOpt::getCallIfRegularCall(U, &RFI);
-      if (!RTCall)
-        return false;
-      MemTransferCalls.push_back(RTCall);
-      return false;
-    };
-    RFI.foreachUse(GetCallSites);
-    EXPECT_FALSE(MemTransferCalls.empty());
+  HideMemTransferLatencyTest()
+      : OpenMPOptTest(), FAM(true), MAM(true), CGAM(true) {
+    MAM.registerPass([] { return LazyCallGraphAnalysis(); });
+    FAM.registerPass([] { return TargetLibraryAnalysis(); });
+    FAM.registerPass([]{ return AAManager(); });
+    FAM.registerPass([]{ return DominatorTreeAnalysis(); });
+    FAM.registerPass([]{ return MemorySSAAnalysis(); });
+    CGAM.registerPass([&] { return FunctionAnalysisManagerCGSCCProxy(); });
+
+    // Register required pass instrumentation analysis.
+    FAM.registerPass([] { return PassInstrumentationAnalysis(); });
+    MAM.registerPass([] { return PassInstrumentationAnalysis(); });
+    CGAM.registerPass([] {return PassInstrumentationAnalysis(); });
+
+    // Cross-register proxies.
+    MAM.registerPass([&] { return FunctionAnalysisManagerModuleProxy(FAM); });
+    MAM.registerPass([&] { return CGSCCAnalysisManagerModuleProxy(CGAM); });
+    CGAM.registerPass([&] { return ModuleAnalysisManagerCGSCCProxy(MAM); });
+    FAM.registerPass([&] { return CGSCCAnalysisManagerFunctionProxy(CGAM); });
+    FAM.registerPass([&] { return ModuleAnalysisManagerFunctionProxy(MAM); });
+
+//    FunctionPassManager FPM;
+//    FPM.addPass(MFP.getPass());
+
+    CGSCCPassManager CGPM;
+    CGPM.addPass(MSCCP.getPass());
+//    CGPM.addPass(createCGSCCToFunctionPassAdaptor(std::move(FPM)));
+
+    //MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
+    MPM.addPass(createModuleToPostOrderCGSCCPassAdaptor(std::move(CGPM)));
   }
 };
 
-//TEST_F(HideMemTransferLatencyTest, GetValuesInOfflArrays) {
-//  // TODO: Update this ModuleString.
-//  const char *ModuleString = "";
-//  getCallSites(ModuleString);
-//
-//  EXPECT_TRUE(true);
-//}
+TEST_F(HideMemTransferLatencyTest, GetValuesInOfflArrays) {
+  const char *ModuleString =
+      "@.offload_maptypes = private unnamed_addr constant [1 x i64] [i64 33]\n"
+      "define dso_local i32 @dataTransferOnly(double* noalias %a, i32 %size) {\n"
+      "entry:\n"
+      "  %.offload_baseptrs = alloca [1 x i8*], align 8\n"
+      "  %.offload_ptrs = alloca [1 x i8*], align 8\n"
+      "  %.offload_sizes = alloca [1 x i64], align 8\n"
+
+      "  %call = call i32 @rand()\n"
+
+      "  %conv = zext i32 %size to i64\n"
+      "  %0 = shl nuw nsw i64 %conv, 3\n"
+      "  %1 = getelementptr inbounds [1 x i8*], [1 x i8*]* %.offload_baseptrs, i64 0, i64 0\n"
+      "  %2 = bitcast [1 x i8*]* %.offload_baseptrs to double**\n"
+      "  store double* %a, double** %2, align 8\n"
+      "  %3 = getelementptr inbounds [1 x i8*], [1 x i8*]* %.offload_ptrs, i64 0, i64 0\n"
+      "  %4 = bitcast [1 x i8*]* %.offload_ptrs to double**\n"
+      "  store double* %a, double** %4, align 8\n"
+      "  %5 = getelementptr inbounds [1 x i64], [1 x i64]* %.offload_sizes, i64 0, i64 0\n"
+      "  store i64 %0, i64* %5, align 8\n"
+      "  call void @__tgt_target_data_begin(i64 -1, i32 1, i8** nonnull %1, i8** nonnull %3, i64* nonnull %5, i64* getelementptr inbounds ([1 x i64], [1 x i64]* @.offload_maptypes, i64 0, i64 0))\n"
+
+      "  %rem = urem i32 %call, %size\n"
+
+      "  call void @__tgt_target_data_end(i64 -1, i32 1, i8** nonnull %1, i8** nonnull %3, i64* nonnull %5, i64* getelementptr inbounds ([1 x i64], [1 x i64]* @.offload_maptypes, i64 0, i64 0))\n"
+      "  ret i32 %rem\n"
+      "}\n"
+
+      "declare dso_local void @__tgt_target_data_begin(i64, i32, i8**, i8**, i64*, i64*)\n"
+      "declare dso_local void @__tgt_target_data_end(i64, i32, i8**, i8**, i64*, i64*)\n"
+      "declare dso_local i32 @rand()";
+
+  std::unique_ptr<Module> M = parseModuleString(ModuleString);
+
+  EXPECT_CALL(MSCCP, run(_, _, _, _))
+      .WillRepeatedly(Invoke([](LazyCallGraph::SCC &C, CGSCCAnalysisManager &AM,
+                                LazyCallGraph &CG, CGSCCUpdateResult &UR) {
+        SmallPtrSet<Function *, 16> ModuleSlice;
+        SmallVector<Function *, 16> SCC;
+        for (LazyCallGraph::Node &N : C) {
+          SCC.push_back(&N.getFunction());
+          ModuleSlice.insert(SCC.back());
+        }
+
+        EXPECT_FALSE(SCC.empty());
+
+        FunctionAnalysisManager &FAM =
+            AM.getResult<FunctionAnalysisManagerCGSCCProxy>(C, CG).getManager();
+
+        AnalysisGetter AG(FAM);
+        auto OREGetter = [&FAM](Function *F) -> OptimizationRemarkEmitter & {
+          return FAM.getResult<OptimizationRemarkEmitterAnalysis>(*F);
+        };
+
+        CallGraphUpdater CGUpdater;
+        CGUpdater.initialize(CG, C, AM, UR);
+
+        SetVector<Function *> Functions(SCC.begin(), SCC.end());
+        BumpPtrAllocator Allocator;
+        OMPInformationCache InfoCache(*(Functions.back()->getParent()), AG, Allocator,
+            /*CGSCC*/ &Functions, ModuleSlice);
+        Attributor A(Functions, InfoCache, CGUpdater);
+        OpenMPOpt OMPOpt(SCC, CGUpdater, OREGetter, InfoCache, A);
+
+        auto &RFI = InfoCache.RFIs[OMPRTL___tgt_target_data_begin];
+        auto GetValuesInOfflArrays = [&](Use &U, Function &Decl) {
+          auto *RTCall = OpenMPOpt::getCallIfRegularCall(U, &RFI);
+          EXPECT_TRUE(RTCall);
+
+          auto *MSSAResult =
+              InfoCache.getAnalysisResultForFunction<MemorySSAAnalysis>(
+                  *RTCall->getCaller());
+          EXPECT_TRUE(MSSAResult);
+
+          auto &MSSA = MSSAResult->getMSSA();
+          OMPInformationCache::MemoryTransfer MT(RTCall, MSSA);
+          OMPOpt.getValuesInOfflArrays(MT);
+
+          // TODO: Check for the values stored in the offload arrays.
+
+          return false;
+        };
+        RFI.foreachUse(GetValuesInOfflArrays);
+
+        return PreservedAnalyses::all();
+      }));
+
+  MPM.run(*M, MAM);
+}
 
 } // end anonymous namespace
--- llvm/unittests/Transforms/IPO/OpenMPOpt/OpenMPOptTest.h
+++ llvm/unittests/Transforms/IPO/OpenMPOpt/OpenMPOptTest.h
@@ -12,44 +12,81 @@
 #include "llvm/Transforms/IPO/OpenMPOpt.h"
 #include "llvm/AsmParser/Parser.h"
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 using namespace llvm;
 using namespace omp;
 
 namespace {
 
+using testing::NiceMock;
+using testing::Matcher;
+using testing::Return;
+using testing::Invoke;
+using testing::_;
+
+template <typename DerivedT, typename IRUnitT,
+    typename AnalysisManagerT = AnalysisManager<IRUnitT>,
+    typename... ExtraArgTs>
+class MockPassBase {
+public:
+  class Pass : public PassInfoMixin<Pass> {
+    friend MockPassBase;
+
+    DerivedT *Handle;
+
+    Pass(DerivedT &Handle) : Handle(&Handle) {
+      static_assert(std::is_base_of<MockPassBase, DerivedT>::value,
+                    "Must pass the derived type to this template!");
+    }
+
+  public:
+    PreservedAnalyses run(IRUnitT &IR, AnalysisManagerT &AM,
+                          ExtraArgTs... ExtraArgs) {
+      return Handle->run(IR, AM, ExtraArgs...);
+    }
+  };
+
+  Pass getPass() { return Pass(static_cast<DerivedT &>(*this)); }
+
+protected:
+  /// Derived classes should call this in their constructor to set up default
+  /// mock actions. (We can't do this in our constructor because this has to
+  /// run after the DerivedT is constructed.)
+  void setDefaults() {
+    ON_CALL(static_cast<DerivedT &>(*this),
+            run(_, _, testing::Matcher<ExtraArgTs>(_)...))
+        .WillByDefault(Return(PreservedAnalyses::all()));
+  }
+};
+
+struct MockFunctionPass
+    : MockPassBase<MockFunctionPass, Function> {
+  MOCK_METHOD2(run, PreservedAnalyses(Function &, FunctionAnalysisManager &));
+
+  MockFunctionPass() { setDefaults(); }
+};
+
+struct MockModulePass : MockPassBase<MockModulePass, Module> {
+  MOCK_METHOD2(run, PreservedAnalyses(Module &, ModuleAnalysisManager &));
+
+  MockModulePass() { setDefaults(); }
+};
+
+
+struct MockSCCPass : MockPassBase<MockSCCPass, LazyCallGraph::SCC,
+    CGSCCAnalysisManager &, LazyCallGraph &, CGSCCUpdateResult &> {
+  MOCK_METHOD4(run,
+               PreservedAnalyses(LazyCallGraph::SCC &, CGSCCAnalysisManager &,
+                                 LazyCallGraph &, CGSCCUpdateResult &));
+
+};
+
 class OpenMPOptTest : public ::testing::Test {
 protected:
-  std::unique_ptr<OMPInformationCache> OMPInfoCache;
   std::unique_ptr<LLVMContext> Ctx;
 
   OpenMPOptTest() : Ctx(new LLVMContext) {}
-
-  /// Initializes OMPInfoCache attribute cache given a module \p M.
-  /// This must be called before using OMPInfoCache.
-  void initializeOMPInfoCache(Module &M) {
-    CallGraph CG(M);
-    scc_iterator<CallGraph *> CGI = scc_begin(&CG);
-    CallGraphSCC CGSCC(CG, &CGI);
-
-    SmallPtrSet<Function *, 16> ModuleSlice;
-    SmallVector<Function *, 16> SCC;
-    for (CallGraphNode *CGN : CGSCC)
-      if (Function *Fn = CGN->getFunction())
-        if (!Fn->isDeclaration()) {
-          SCC.push_back(Fn);
-          ModuleSlice.insert(Fn);
-        }
-
-    EXPECT_FALSE(SCC.empty());
-
-    AnalysisGetter AG;
-    SetVector<Function *> Functions(SCC.begin(), SCC.end());
-    BumpPtrAllocator Allocator;
-    OMPInfoCache = std::make_unique<OMPInformationCache>(
-        *(Functions.back()->getParent()), AG, Allocator, /*CGSCC*/ &Functions,
-        ModuleSlice);
-  }
 
   std::unique_ptr<Module> parseModuleString(const char *ModuleString) {
     SMDiagnostic Err;
